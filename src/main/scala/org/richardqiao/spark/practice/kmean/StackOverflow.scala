0b73665250f3a7cfe9882995611b81a4013a0fce
richardqiao2000 <richardqiao2000@gmail.com>
2017-04-13 17:39:28 -0500

    kmeans rewritten

--- src/main/scala/org/richardqiao/spark/practice/kmean/StackOverflow.scala
+++ src/main/scala/org/richardqiao/spark/practice/kmean/StackOverflow.scala
@@ -30,6 +30,7 @@ object StackOverflow{
   val kmeansKernels = langs.length * 3
   val kmeansETA = 20.0D
   val kmeansMaxIterations = 120
+  val kmeansConvergence = 20.0D
   
   assert(langSpread > 0, "If langSpread is zero we can't recover the language from the input data")
   
@@ -70,7 +71,7 @@ object StackOverflow{
     val vectors = scores.map(s => ((if(s._1.tags == None) 0 else langs.indexOf(s._1.tags.get)) * langSpread, s._2))
     assert(vectors.count == 2121822, "Incorrect number of vector: " + vectors.count)
     
-    //5. Build initial sample cluster points by using reservoir sampling
+    //5. Build initial sample cluster kernels by using reservoir sampling
     //val samples = sampleVectors(vectors)
     val samples = vectors.groupByKey.flatMap({case (lang, list) => 
                                            sampling(list.toIterator, 3).map((lang, _))
@@ -78,15 +79,71 @@ object StackOverflow{
     
                                          
     //6. Iterate do KMeans algorithm
-    val means = kmeans(samples, vectors, debug = true)
+    val means = kmeans2(samples, vectors, 0)
     
     //7. Collect Cluster results
     val results = clusterResults(means, vectors)
     printResults(results)
     
-    //samples.foreach(println)
+    //means.foreach(println)
   }
   
+  def distance(p1: (Int, Int), p2: (Int, Int)): Long = {
+    val xx = (p1._1 - p2._1).toLong * (p1._1 - p2._1).toLong
+    val yy = (p1._2 - p2._2).toLong * (p1._2 - p2._2).toLong
+    xx + yy
+  }
+  
+  def getClosestKernel(p: (Int, Int), means: Array[(Int, Int)]): Int = {
+    var index = 0
+    var minDist = Long.MaxValue
+    for(i <- 0 until means.length){
+      val dist = distance(p, means(i))
+      if(minDist > dist){
+        minDist = dist
+        index = i
+      }
+    }
+    index
+  }
+  
+  def getCenter(list: Iterable[(Int, Int)]): (Int, Int) = {
+    var x: Long = 0
+    var y: Long = 0
+    val len = list.iterator.length
+    for(p <- list){
+      x += p._1
+      y += p._2
+    }
+    ((x / len).toInt, (y / len).toInt)
+  }
+  
+  def kmeans2(lastMean: Array[(Int, Int)], vectors: RDD[(Int, Int)], iter: Int = 1): Array[(Int, Int)] = {
+    val next = lastMean.clone
+    //1. Get next mean
+    val newMeans = vectors.map(v => (getClosestKernel(v, next), v))
+                      .groupByKey
+                      .mapValues(getCenter)
+                      .collect
+    newMeans.foreach(kv => next.update(kv._1, kv._2))
+
+    //2. getDistance away from last means
+    var dist: Double = 0
+    for(i <- 0 until next.length){
+      val p1 = lastMean(i)
+      val p2 = next(i)
+      val xx: Double = (p1._1 - p2._1).toDouble
+      val yy: Double = (p1._2 - p2._2).toDouble
+      dist += xx * xx + yy * yy
+    }
+    
+    //3. Call next cycle of kmeans or return
+    if(dist < kmeansConvergence || iter > kmeansMaxIterations){
+      next
+    }else{
+      kmeans2(next, vectors, iter + 1)
+    }
+  }
 
   def sampling(list: Iterator[Int], size: Int): Array[Int] = {
     val res = new Array[Int](size)
@@ -105,122 +162,10 @@ object StackOverflow{
     }
     res
   }
-  
-
-  /** Main kmeans computation */
-  @tailrec final def kmeans(means: Array[(Int, Int)], vectors: RDD[(Int, Int)], iter: Int = 1, debug: Boolean = false): Array[(Int, Int)] = {
-    val newMeans = means.clone() // you need to compute newMeans
-
-    // TODO: Fill in the newMeans array
-    val tmpMeans = vectors
-      .map { p =>
-        (findClosest(p, means), p)
-      }
-      .groupByKey()
-      .mapValues(averageVectors)
-      .collect()
-
-    tmpMeans foreach (kv => newMeans update(kv._1, kv._2))
-
-    val distance = euclideanDistance(means, newMeans)
-
-    if (debug) {
-      println(s"""Iteration: $iter
-                 |  * current distance: $distance
-                 |  * desired distance: $kmeansETA
-                 |  * means:""".stripMargin)
-      for (idx <- 0 until kmeansKernels)
-      println(f"   ${means(idx).toString}%20s ==> ${newMeans(idx).toString}%20s  " +
-              f"  distance: ${euclideanDistance(means(idx), newMeans(idx))}%8.0f")
-    }
-
-    if (converged(distance))
-      newMeans
-    else if (iter < kmeansMaxIterations)
-      kmeans(newMeans, vectors, iter + 1, debug)
-    else {
-      println("Reached max iterations!")
-      newMeans
-    }
-  }
-
-
-  /** Decide whether the kmeans clustering converged */
-  def converged(distance: Double) =
-    distance < kmeansETA
-
-
-  /** Return the euclidean distance between two points */
-  def euclideanDistance(v1: (Int, Int), v2: (Int, Int)): Double = {
-    val part1 = (v1._1 - v2._1).toDouble * (v1._1 - v2._1)
-    val part2 = (v1._2 - v2._2).toDouble * (v1._2 - v2._2)
-    part1 + part2
-  }
-
-  /** Return the euclidean distance between two points */
-  def euclideanDistance(a1: Array[(Int, Int)], a2: Array[(Int, Int)]): Double = {
-    assert(a1.length == a2.length)
-    var sum = 0d
-    var idx = 0
-    while(idx < a1.length) {
-      sum += euclideanDistance(a1(idx), a2(idx))
-      idx += 1
-    }
-    sum
-  }
-
-  /** Return the closest point */
-  def findClosest(p: (Int, Int), centers: Array[(Int, Int)]): Int = {
-    var bestIndex = 0
-    var closest = Double.PositiveInfinity
-    for (i <- 0 until centers.length) {
-      val tempDist = euclideanDistance(p, centers(i))
-      if (tempDist < closest) {
-        closest = tempDist
-        bestIndex = i
-      }
-    }
-    bestIndex
-  }
-
-
-  /** Average the vectors */
-  def averageVectors(ps: Iterable[(Int, Int)]): (Int, Int) = {
-    val iter = ps.iterator
-    var count = 0
-    var comp1: Long = 0
-    var comp2: Long = 0
-    while (iter.hasNext) {
-      val item = iter.next
-      comp1 += item._1
-      comp2 += item._2
-      count += 1
-    }
-    ((comp1 / count).toInt, (comp2 / count).toInt)
-  }
-
-
-
-
   def clusterResults(means: Array[(Int, Int)], vectors: RDD[(Int, Int)]): Array[(String, Double, Int, Int)] = {
-    val closest = vectors.map(p => (findClosest(p, means), p))
-    val closestGrouped = closest.groupByKey
+    val closestGrouped = vectors.map(p => (getClosestKernel(p, means), p)).groupByKey
 
     val median = closestGrouped.mapValues { vs =>
-      //val langMap = vs.groupBy(_._1)
-      /*
-      val mostCommon = vs.groupBy(_._1).maxBy(qa => qa._2.size)
-      val langLabel: String = langs(mostCommon._1 / langSpread) // most common language in the cluster
-      val clusterSize: Int = vs.size
-      val langPercent: Double = mostCommon._2.size.toDouble * 100 / clusterSize // percent of the questions in the most common language
-      val sortedScores = vs.map(_._2).toVector.sorted
-      val medianScore: Int =
-        if (clusterSize % 2 == 1) sortedScores(clusterSize / 2)
-        else (sortedScores(clusterSize / 2) + sortedScores(clusterSize / 2 - 1)) / 2
-        * 
-        */
-
-      
       val m = vs.groupBy(_._1).maxBy(qa => qa._2.size)
       val langLabel: String   = langs(m._1 / langSpread) // most common language in the cluster
       val langPercent: Double =  m._2.size.toDouble * 100 / vs.size // percent of the questions in the most common language
